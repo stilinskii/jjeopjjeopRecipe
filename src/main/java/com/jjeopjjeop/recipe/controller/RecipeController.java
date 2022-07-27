3f0258acf22a0d6abaaf9562e3897fc211c9bdfe
Bdhyeon <bdhn1100@gmail.com>
2022-07-27 11:59:29 +0900

    ADD: RecipeController.java

---
+++ src/main/java/com/jjeopjjeop/recipe/controller/RecipeController.java
@@ -0,0 +1,87 @@
+package com.jjeopjjeop.recipe.controller;
+
+public class RecipeController {
+
+    // 레시피 목록 조회 메소드
+    public String rcpListMethod(){
+
+        return null;
+    }
+
+    // 레시피 목록 검색 메소드
+    public String rcpSearchMethod(){
+
+        return null;
+    }
+
+    // 레시피 본문 조회 메소드
+    public String rcpViewMethod(){
+
+        return null;
+    }
+
+    // 레시피 스크랩 메소드
+    public String rcpScrapMethod(){
+
+        return null;
+    }
+
+    // 레시피 신고 메소드
+    public String rcpReportMethod(){
+
+        return null;
+    }
+
+    // 레시피 작성 페이지 요청 메소드
+//    public String rcpWriteMethod(){
+//
+//    }
+
+    // 레시피 작성 메소드
+    public String rcpWriteMethod(){
+
+        return null;
+    }
+
+    // 레시피 수정 페이지 요청 메소드
+//    public String rcpUpdateMethod(){
+//
+//    }
+
+    // 레시피 수정 메소드
+    public String rcpUpdateMethod(){
+
+        return null;
+    }
+
+    // 레시피 삭제 메소드
+    public String rcpDeleteMethod(){
+
+        return null;
+    }
+
+    //////////// 레시피 댓글 관리 ////////////
+    // 레시피 댓글 작성 메소드
+    public String rcpCoWriteMethod(){
+
+        return null;
+    }
+
+    // 레시피 댓글 수정 메소드
+    public String rcpCoUpdateMethod(){
+
+        return null;
+    }
+
+    // 레시피 댓글 삭제 메소드
+    public String rcpCoDeleteMethod(){
+
+        return null;
+    }
+
+    // 레시피 댓글 신고 메소드
+    public String rcpCoReportMethod(){
+
+        return null;
+    }
+}
