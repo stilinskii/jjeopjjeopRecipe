413df8fb2be489520d4a0150c399be8b9964c2d5
Bdhyeon <bdhn1100@gmail.com>
2022-07-27 13:05:36 +0900

    CHG: update methods at RecipeController.java

--- src/main/java/com/jjeopjjeop/recipe/controller/RecipeController.java
+++ src/main/java/com/jjeopjjeop/recipe/controller/RecipeController.java
@@ -1,87 +1,105 @@
 package com.jjeopjjeop.recipe.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
 public class RecipeController {
 
     // 레시피 목록 조회 메소드
+    @GetMapping("/recipe/list")
     public String rcpListMethod(){
 
-        return null;
+        return "/recipe/rcpList";
     }
 
     // 레시피 목록 검색 메소드
+    @GetMapping("/recipe/search")
     public String rcpSearchMethod(){
 
-        return null;
+        return "/recipe/rcpSearch";
     }
 
     // 레시피 본문 조회 메소드
+    @GetMapping("/recipe/view")
     public String rcpViewMethod(){
 
-        return null;
+        return "/recipe/rcpView";
     }
 
     // 레시피 스크랩 메소드
+    @PutMapping("/recipe/scrap")
     public String rcpScrapMethod(){
 
-        return null;
+        return "redirect:/recipe/view";
     }
 
     // 레시피 신고 메소드
+    @PutMapping("/recipe/report")
     public String rcpReportMethod(){
 
-        return null;
+        return "redirect:/recipe/view";
     }
 
     // 레시피 작성 페이지 요청 메소드
-//    public String rcpWriteMethod(){
-//
-//    }
-
-    // 레시피 작성 메소드
+    @GetMapping("/recipe/write")
     public String rcpWriteMethod(){
 
-        return null;
+        return "/recipe/rcpWrite";
     }
 
-    // 레시피 수정 페이지 요청 메소드
-//    public String rcpUpdateMethod(){
+    // 레시피 작성 메소드
+//    public String rcpWriteMethod(){
 //
+//        return null;
 //    }
 
-    // 레시피 수정 메소드
+    // 레시피 수정 페이지 요청 메소드
+    @GetMapping("/recipe/update")
     public String rcpUpdateMethod(){
 
-        return null;
+        return "/recipe/rcpUpdate";
     }
 
+    // 레시피 수정 메소드
+//    public String rcpUpdateMethod(){
+//
+//        return null;
+//    }
+
     // 레시피 삭제 메소드
+    @DeleteMapping("/recipe/delete")
     public String rcpDeleteMethod(){
 
-        return null;
+        return "redirect:/recipe/list";
     }
 
     //////////// 레시피 댓글 관리 ////////////
     // 레시피 댓글 작성 메소드
+    @PostMapping("/recipe/comment")
     public String rcpCoWriteMethod(){
 
-        return null;
+        return "redirect:/recipe/view";
     }
 
     // 레시피 댓글 수정 메소드
+    @PutMapping("/recipe/comment/update")
     public String rcpCoUpdateMethod(){
 
-        return null;
+        return "redirect:/recipe/view";
     }
 
     // 레시피 댓글 삭제 메소드
+    @DeleteMapping("/recipe/comment/delete")
     public String rcpCoDeleteMethod(){
 
-        return null;
+        return "redirect:/recipe/view";
     }
 
     // 레시피 댓글 신고 메소드
+    @PutMapping("/recipe/comment/report")
     public String rcpCoReportMethod(){
 
-        return null;
+        return "redirect:/recipe/view";
     }
 }
