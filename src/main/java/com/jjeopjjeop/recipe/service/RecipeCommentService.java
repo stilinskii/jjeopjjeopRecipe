d64868df8c9c7fee2673c8dcc29bf130ef948239
Bdhyeon <bdhn1100@gmail.com>
2022-08-04 16:06:11 +0900

    ADD: 덧글 삭제 기능 추가

--- src/main/java/com/jjeopjjeop/recipe/service/RecipeCommentService.java
+++ src/main/java/com/jjeopjjeop/recipe/service/RecipeCommentService.java
@@ -8,7 +8,6 @@ import java.util.List;
 public interface RecipeCommentService {
     public int countProcess(int rcp_seq);
     public List<RecipeCommentDTO> listProcess(int rcp_seq);
-
     public void writeProcess(RecipeCommentDTO recipeCommentDTO);
     public void deleteProcess(int co_rcp_seq);
 }
