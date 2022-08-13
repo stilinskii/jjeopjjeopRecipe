d6228c789c6bfd07b3f5527b964a8310c0d4a5b2
Bdhyeon <bdhn1100@gmail.com>
2022-08-13 17:19:41 +0900

    덧글 작성 시 rcp_sort, cate_seq 값 초기화되는 것 수정

--- src/main/java/com/jjeopjjeop/recipe/controller/RecipeCommentController.java
+++ src/main/java/com/jjeopjjeop/recipe/controller/RecipeCommentController.java
@@ -16,18 +16,20 @@ public class RecipeCommentController {
     private int currentPage;
 
     @PostMapping("/recipe/comment/write")
-    public String writeMethod(int rcp_seq, Integer currentPage, RecipeCommentDTO recipeCommentDTO){
+    public String writeMethod(int rcp_seq, Integer currentPage, int rcp_sort, int cate_seq, String searchKey,
+                              RecipeCommentDTO recipeCommentDTO){
         recipeCommentDTO.setRcp_seq(rcp_seq);
         recipeCommentDTO.setUser_id("테스트");
         service.writeProcess(recipeCommentDTO);
 
-        return "redirect:/recipe/view/"+rcp_seq+"?currentPage="+currentPage;
+        return "redirect:/recipe/view/"+rcp_seq+"?rcp_sort="+rcp_sort+"&cate_seq="+cate_seq+"&currentPage="+currentPage+"&searchKey="+searchKey;
     }
 
     @GetMapping("/recipe/comment/delete")
-    public String deleteMethod(Integer rcp_seq, Integer currentPage, Integer co_rcp_seq){
+    public String deleteMethod(int rcp_seq, Integer currentPage, int rcp_sort, int cate_seq, String searchKey,
+                               Integer co_rcp_seq){
 
         service.deleteProcess(co_rcp_seq);
-        return "redirect:/recipe/view/"+rcp_seq+"?currentPage="+currentPage;
+        return "redirect:/recipe/view/"+rcp_seq+"?rcp_sort="+rcp_sort+"&cate_seq="+cate_seq+"&currentPage="+currentPage+"&searchKey="+searchKey;
     }
 }
