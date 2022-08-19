4f2e91cecd47a08c8ff51b71f8845301ca034c3a
MJ <mjkimthebest@gmail.com>
2022-08-20 00:23:45 +0900

    Modify Main Application

--- src/main/java/com/jjeopjjeop/recipe/ServletInitializer.java
+++ src/main/java/com/jjeopjjeop/recipe/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(ProduceApplication.class);
+		return application.sources(RecipeApplication.class);
 	}
 
 }
