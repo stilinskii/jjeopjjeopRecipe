1b3eb7958592972881576bdd10d26629dba2c93f
Bdhyeon <bdhn1100@gmail.com>
2022-08-22 22:21:32 +0900

    RecipeCommentController 수정

--- src/main/java/com/jjeopjjeop/recipe/controller/RecipeCommentController.java
+++ src/main/java/com/jjeopjjeop/recipe/controller/RecipeCommentController.java
@@ -10,12 +10,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpSession;
-import java.util.List;
 
 @Controller
 public class RecipeCommentController {
