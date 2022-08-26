5c380f0a0eeb2f89783b13cabe6df5e8e1f0f79a
stilinskii <lhy1031hy@gmail.com>
2022-08-26 20:02:03 +0900

    FIX: 글쓰기 안됨 오류

--- src/main/java/com/jjeopjjeop/recipe/dto/CommunityCommentDTO.java
+++ src/main/java/com/jjeopjjeop/recipe/dto/CommunityCommentDTO.java
@@ -1,8 +1,6 @@
 package com.jjeopjjeop.recipe.dto;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.stereotype.Component;
 
 import javax.validation.constraints.NotNull;
