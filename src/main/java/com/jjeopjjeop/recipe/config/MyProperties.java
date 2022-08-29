25735297d6d887dae41b53b446b0d1d0dc98aaea
MJ <mjkimthebest@gmail.com>
2022-08-29 18:27:11 +0900

    Edit findPassword condition

--- src/main/java/com/jjeopjjeop/recipe/config/MyProperties.java
+++ src/main/java/com/jjeopjjeop/recipe/config/MyProperties.java
@@ -4,22 +4,22 @@ import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-@Data
-@Component
-public class MyProperties {
-
-   //인증메일 변수
-   @Value("${spring.mail.host}")
-   private String mailHost;
-   @Value("${spring.mail.port}")
-   private String mailPort;
-   @Value("${spring.mail.username}")
-   private String mailUsername;
-   @Value("${spring.mail.password}")
-   private String mailPassword;
-   @Value("${spring.mail.properties.mail.smtp.auth}")
-   private String mailAuth;
-   @Value("${spring.mail.properties.mail.smtp.starttls.enable}")
-   private String mailEnable;
-
-}
+//@Data
+//@Component
+//public class MyProperties {
+//
+//   //인증메일 변수
+//   @Value("${spring.mail.host}")
+//   private String mailHost;
+//   @Value("${spring.mail.port}")
+//   private String mailPort;
+//   @Value("${spring.mail.username}")
+//   private String mailUsername;
+//   @Value("${spring.mail.password}")
+//   private String mailPassword;
+//   @Value("${spring.mail.properties.mail.smtp.auth}")
+//   private String mailAuth;
+//   @Value("${spring.mail.properties.mail.smtp.starttls.enable}")
+//   private String mailEnable;
+//
+//}
